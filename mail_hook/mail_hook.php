fec7e7e5e4014e71ec433986733f46a7c89ed412
Kahiro-M <0204.kahiro@gmail.com>
2024-05-16 16:55:24 +0900

    出力テキストファイルに差出人と受信者を追加

--- mail_hook/mail_hook.php
+++ mail_hook/mail_hook.php
@@ -8,11 +8,17 @@ qd_receive_mail( 'stdin' );
 //メール件名
 $subject = qd_receive_mail( 'header' , array('subject','name') );
  
+//送信者
+$from = qd_receive_mail( 'header' , array('from','mail') );
+
+//受信者
+$to = qd_receive_mail( 'header' , array('to','mail') );
+ 
 //メール本文の取得
 $body = qd_receive_mail( 'body' );
  
 $fp = fopen('mail_simple.txt', 'w');
-fwrite($fp, "【件名】\n".$subject."\n\n------\n\n【本文】\n".$body );
+fwrite($fp, "【差出人】\n".$from."\n\n------\n\n【受信者】\n".$to."\n\n------\n\n【件名】\n".$subject."\n\n------\n\n【本文】\n".$body );
 fclose($fp);
  
 
